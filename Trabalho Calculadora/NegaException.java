b2ab430513a9521f157560fbca89ca96d9171c7d
Jair Brann Junior <jairjunior02@hotmail.com>
2022-03-28 22:29:46 -0300

    Commit calculadora

---
+++ Trabalho Calculadora/NegaException.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class NegaException extends Exception {
+	NegaException(int numero) {
+		JOptionPane.showMessageDialog(null, "Numero negativo!","Aviso",JOptionPane.PLAIN_MESSAGE);
+	}
+}
